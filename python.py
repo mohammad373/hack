a0864f24da0d5b02e3e71375f33f917af34210dc
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-03 20:33:45 +0330

    Update python.py

--- python.py
+++ python.py
@@ -5,7 +5,9 @@ print("hello welcome back ;)")
 a = int(input("Enter Your Number One ==>  "))
 b =  int(input("Enter Your Number Two ==>  "))
 x = a ** b
+print("your number is {x}")
+c = int(input("Enter Your Number For Turn Off ==>  "))
+os.system(f"shutdown /s /t {c}")
 
 
 
-print(Fore.Red+f"your number is {x}")
